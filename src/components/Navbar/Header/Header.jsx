40e746f964264443950dfd92a91e29fe770769c9
ankitwjha <ajha76912@gmail.com>
2026-04-04 00:54:06 +0530

    Initial commit

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>
+            Order Food On The Go!
+        </h2>
+        <p>
+             A diverse menu that caters to every taste and preference. From light bites to hearty meals, vegetarian choices to indulgent treats, the variety ensures that everyone finds something they’ll love. With options spanning across different styles and dietary needs, our menu is designed to bring convenience, freshness, and satisfaction straight to your doorstep.
+
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
