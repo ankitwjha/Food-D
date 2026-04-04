3c758cac7f509f9637fbd3e943d7b8fb4bd22fcc
ankitwjha <ajha76912@gmail.com>
2026-04-04 15:30:59 +0530

    fin

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,22 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
+
+// export const connectDB = async () => {
+//     await mongoose.connect('mongodb+srv://ankitjha1978:19782005@cluster0.9k0pvoo.mongodb.net/food-d')
+//     .then (()=>console.log("DB connected"))
+// }
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ankitjha1978:19782005@cluster0.9k0pvoo.mongodb.net/food-d')
-    .then (()=>console.log("DB connected"))
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected");
+  } catch (err) {
+    console.error("DB connection failed:", err);
+  }
+};
